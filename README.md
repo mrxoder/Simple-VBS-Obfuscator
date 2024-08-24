8007f866e09e296d066c6fc20761d96d18b290af
xoder <xoderxoderx@gmail.com>
2024-08-24 04:40:45 -0400

    vbs obfuscator

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ I encountered an obfuscated VBS payload.
 I reverse-engineered the obfuscation algorithm and 
 then attempted to recreate the Python script used to obfuscate the VBS payload.
 
-``Usage:  vbs_obf.py input_vbs_filename   output_vbs_filename``
+``Usage: python vbs_obf.py input_vbs_filename   output_vbs_filename``
--- vbs_obf.py
+++ vbs_obf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 
 camouflage = "\'Your use of this software is governed by the terms of the license agreement under which you acquired this software. If you are a volume licensing customer, the use of this software is governed by your volume license agreement. You are not authorized to use this software if you have not acquired a valid license for the software from Microsoft or one of its authorized distributors.\n\'Copyright (C) 2015"
@@ -16,24 +17,25 @@ def obfuscate(s):
     out = ''.join(format(ord(c), '02x') for c in s)
     return out
 
-if len( sys.argv ) < 3:
-   print("Usage: ", sys.argv[0], "input_vbs_filename   output_vbs_filename")
-   sys.exit()
 
+if __name__ == "__main__":
+    if len( sys.argv ) < 3:
+        print("Usage: python ", sys.argv[0], "input_vbs_filename   output_vbs_filename")
+        sys.exit()
 
-in_file = open( sys.argv[1] )
-original_string = in_file.read()
-out_string = obfuscate( original_string )
+    in_file = open( sys.argv[1] )
+    original_string = in_file.read()
+    out_string = obfuscate( original_string )
 
-chunks = []
-for i in range( 0, len(out_string), 256):
-    chunks.append( payload_name + "=" + payload_name + "&"+ func_name +"(\"" + out_string[ i:i+256 ] + "\")" )
+    chunks = []
+    for i in range( 0, len(out_string), 256):
+        chunks.append( payload_name + "=" + payload_name + "&"+ func_name +"(\"" + out_string[ i:i+256 ] + "\")" )
 
-out_file = open( sys.argv[2], "w" )
-out_file.write( camouflage+"\n")
-out_file.write( payload_name+"=\"\"\n" )
-out_file.write( '\n'.join( chunks ) )
-out_file.write( "\n"+ func0_name + " "+ payload_name )
-out_file.write( executor )
-out_file.write( deobfuc_vbs )
-out_file.close()
+    out_file = open( sys.argv[2], "w" )
+    out_file.write( camouflage+"\n")
+    out_file.write( payload_name+"=\"\"\n" )
+    out_file.write( '\n'.join( chunks ) )
+    out_file.write( "\n"+ func0_name + " "+ payload_name )
+    out_file.write( executor )
+    out_file.write( deobfuc_vbs )
+    out_file.close()
